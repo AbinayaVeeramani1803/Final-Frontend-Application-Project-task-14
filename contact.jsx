c82223a78fd7ef859e2dec6e396b7533e560bb0a
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-30 21:05:47 +0530

    contact.jsx

---
+++ contact.jsx
@@ -0,0 +1,23 @@
+import "./Contact.css";
+
+function Contact(){
+
+return(
+
+<div className="contact">
+
+<h1>Contact Us</h1>
+
+<p>Email : travelone@gmail.com</p>
+
+<p>Phone : +91 9876543210</p>
+
+<p>Address : Chennai, Tamil Nadu</p>
+
+</div>
+
+)
+
+}
+
+export default Contact;
