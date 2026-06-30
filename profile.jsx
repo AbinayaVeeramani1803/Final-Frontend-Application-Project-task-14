553687982cc49ab71abb140d817b80f510295254
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-30 21:07:59 +0530

    profile.jsx

---
+++ profile.jsx
@@ -0,0 +1,28 @@
+import "./Profile.css";
+
+function Profile(){
+
+return(
+
+<div className="profile">
+
+<img
+src="https://i.pravatar.cc/180"
+alt="profile"
+/>
+
+<h2>Abinaya Veeramani</h2>
+
+<p>Traveler</p>
+
+<p>Email : abinaya@gmail.com</p>
+
+<button>Edit Profile</button>
+
+</div>
+
+)
+
+}
+
+export default Profile;
