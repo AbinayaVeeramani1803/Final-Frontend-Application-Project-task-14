df91cb43475b738236e4739af5ccb4188c7c6afd
Props Implementation in React Task 2 <abinayaveeramani@thestackly.com>
2026-06-30 21:06:53 +0530

    home.jsx

---
+++ home.jsx
@@ -0,0 +1,21 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="hero">
+
+      <div className="overlay">
+
+        <h1>Explore The World</h1>
+
+        <p>Book your dream vacation today</p>
+
+        <button>Explore Now</button>
+
+      </div>
+
+    </div>
+  );
+}
+
+export default Home;
